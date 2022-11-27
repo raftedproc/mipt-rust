4853cb7acb09cd4761a056b3a2040a8d9eed3ec8
drrtuy <drrtuy@ya.ru>
2022-11-27 17:22:44 +0300

    FS walker problem on different Fn types

--- problems/macros/fswalk/src/handle.rs
+++ problems/macros/fswalk/src/handle.rs
@@ -1,5 +1,10 @@
 #![forbid(unsafe_code)]
 
+use std::{
+    marker::PhantomData,
+    path::{Path, PathBuf},
+};
+
 pub enum Handle<'a> {
     Dir(DirHandle<'a>),
     File(FileHandle<'a>),
@@ -11,32 +16,40 @@ pub enum Handle<'a> {
 
 pub struct DirHandle<'a> {
     // TODO: your code goes here.
+    pub entry_: &'a std::fs::DirEntry,
+    // pub u_: PhantomData<&'a u32>,
+    pub path_: PathBuf,
+    pub descent_: bool,
 }
 
 impl<'a> DirHandle<'a> {
     pub fn descend(&mut self) {
         // TODO: your code goes here.
-        unimplemented!()
+        self.descent_ = true;
     }
 
     pub fn path(&self) -> &Path {
         // TODO: your code goes here.
-        unimplemented!()
+        self.path_.as_path()
     }
 }
 
 pub struct FileHandle<'a> {
     // TODO: your code goes here.
+    pub entry_: &'a std::fs::DirEntry,
+    pub path_: PathBuf,
+    // pub content_: Vec<u8>,
+    pub read_: bool,
 }
 
 impl<'a> FileHandle<'a> {
     pub fn read(&mut self) {
         // TODO: your code goes here.
-        unimplemented!()
+        self.read_ = true;
     }
 
     pub fn path(&self) -> &Path {
         // TODO: your code goes here.
-        unimplemented!()
+        self.path_.as_path()
     }
 }
--- problems/macros/fswalk/src/walker.rs
+++ problems/macros/fswalk/src/walker.rs
@@ -1,5 +1,13 @@
 #![forbid(unsafe_code)]
-use crate::handle::{DirHandle, FileHandle, Handle};
+use crate::handle::{
+    DirHandle, FileHandle, Handle,
+    Handle::{Content, Dir, File},
+};
+use std::{
+    collections::HashSet,
+    io::{self, ErrorKind},
+    path::{Path, PathBuf},
+};
 
 type Callback<'a> = dyn FnMut(&mut Handle) + 'a;
 
@@ -11,7 +19,7 @@ pub struct Walker<'a> {
 impl<'a> Walker<'a> {
     pub fn new() -> Self {
         // TODO: your code goes here.
-        unimplemented!()
+        Self { callbacks: vec![] }
     }
 
     pub fn add_callback<F>(&mut self, callback: F)
@@ -19,12 +27,86 @@ impl<'a> Walker<'a> {
         F: FnMut(&mut Handle) + 'a,
     {
         // TODO: your code goes here.
-        unimplemented!()
+        self.callbacks.push(Box::new(callback));
     }
 
-    pub fn walk<P: AsRef<Path>>(mut self, path: P) -> io::Result<()> {
+    pub fn add_boxed_callback<F>(&mut self, callback: Box<F>)
+    where
+        F: FnMut(&mut Handle) + 'a,
+    {
         // TODO: your code goes here.
-        unimplemented!()
+        self.callbacks.push(callback);
+    }
+
+    pub fn walk<P: AsRef<Path>>(&mut self, path: P) -> io::Result<()> {
+        // TODO: your code goes here.
+        if !path.as_ref().exists() && !self.callbacks.is_empty() {
+            return Err(io::Error::new(
+                ErrorKind::NotFound,
+                format!(
+                    "The path {} does not exist.",
+                    path.as_ref().to_str().unwrap()
+                ),
+            ));
+        }
+        if path.as_ref().is_dir() {
+            for entry_res in std::fs::read_dir(path.as_ref().clone())? {
+                let entry = entry_res?;
+                let p = entry.path();
+                let mut dir_paths: HashSet<PathBuf> = HashSet::new();
+                let mut w = Walker::new();
+                if p.is_dir() {
+                    for c in &mut self.callbacks {
+                        let mut dir = Dir {
+                            0: DirHandle {
+                                entry_: &entry,
+                                path_: p.clone(),
+                                descent_: false,
+                            },
+                        };
+                        c(&mut dir);
+                        if let Handle::Dir(handler) = dir {
+                            if handler.descent_ {
+                                dir_paths.insert(p.clone());
+                                w.add_callback(c);
+                            }
+                        }
+                    }
+                } else {
+                    let mut contents: Vec<u8> = vec![];
+                    let mut contents_is_read = false;
+                    for c in &mut self.callbacks {
+                        let mut file = File {
+                            0: FileHandle {
+                                entry_: &entry,
+                                path_: p.clone(),
+                                read_: false,
+                            },
+                        };
+                        c(&mut file);
+                        if let Handle::File(handler) = file {
+                            if handler.read_ {
+                                contents_is_read = if !contents_is_read {
+                                    contents = std::fs::read(handler.path())?;
+                                    true
+                                } else {
+                                    true
+                                };
+                                c(&mut Content {
+                                    file_path: handler.path(),
+                                    content: &contents,
+                                });
+                            }
+                        }
+                    }
+                }
+                println!("walk dir_paths len {:}", dir_paths.len());
+                for p in dir_paths {
+                    w.walk(p)?
+                }
+            }
+        }
+        Ok(())
     }
 
     // TODO: your code goes here.
